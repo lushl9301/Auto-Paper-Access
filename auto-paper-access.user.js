d4bd47f0dad46a248efc8360b280f8228c14d8c1
Lu Shengliang <lushl9301@gmail.com>
2017-08-14 12:27:08 +0800

    add wiley online library

--- auto-paper-access.user.js
+++ auto-paper-access.user.js
@@ -2,7 +2,7 @@
 // @name         Auto Paper Access
 // @updateURL    https://openuserjs.org/meta/lushl9301/Auto_Paper_Access.meta.js
 // @copyright    2017, lushl9301 (https://github.com/lushl9301)
-// @version      0.6
+// @version      0.7
 // @description  A simple script runs on Tampermonkey. You can easily access IEEE Xplore, ACM Digital Library, etc without clicking proxy bookmarklet provided by universities.
 // @author       lushl9301, koallen
 // @license      MIT
@@ -11,6 +11,7 @@
 // @match        http://ieeexplore.ieee.org/*
 // @match        http://dl.acm.org/*
 // @match        https://*.springer.com/*
+// @match        http://onlinelibrary.wiley.com/doi/*
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @grant        GM_getValue
 // @grant        GM_setValue
