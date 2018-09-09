d5e8f1cf7ead02a26c876cc1213f443adaf1b20e
Siyuan Liu <me@shawnliu.me>
2018-09-09 15:42:53 +0800

    allow the button to display after NTU login

--- auto-paper-access.user.js
+++ auto-paper-access.user.js
@@ -2,7 +2,7 @@
 // @name         Auto Paper Access
 // @updateURL    https://openuserjs.org/meta/lushl9301/Auto_Paper_Access.meta.js
 // @copyright    2017, lushl9301 (https://github.com/lushl9301)
-// @version      3.0
+// @version      4.0
 // @description  A simple script runs on Tampermonkey. You can easily access IEEE Xplore, ACM Digital Library, etc without clicking proxy bookmarklet provided by universities.
 // @author       lushl9301, koallen
 // @license      MIT
@@ -14,6 +14,7 @@
 // @match        onlinelibrary.wiley.com/doi/*
 // @match        www.ncbi.nlm.nih.gov/pubmed/*
 // @match        pubsonline.informs.org/*
+// @match        *.ezlibproxy1.ntu.edu.sg/*
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -75,11 +76,13 @@
     document.body.appendChild(button);
 
     // display config or access with uni account
-    if (currUniversity === defaultUniversity)
+    if (currUniversity === defaultUniversity) {
         GM_config.open();
-    else if ("Nanyang Technological University" === currUniversity)
-        location.href = "http://ezlibproxy1.ntu.edu.sg/login?url=" + location.href;
-    else if ('National University of Singapore' == currUniversity)
+    } else if ("Nanyang Technological University" === currUniversity) {
+        if (!location.href.includes("ezlibproxy1.ntu.edu.sg")) {
+            location.href = "https://ezlibproxy1.ntu.edu.sg/login?url=" + location.href;
+        }
+    } else if ('National University of Singapore' == currUniversity) {
         location.href = "http://libproxy1.nus.edu.sg/login?url=" + location.href;
-        // document.body.appendChild(document.createElement('script')).src='http://lgdata.s3-website-us-east-1.amazonaws.com/docs/1035/217629/proxywithga.js';
+    }
 })();
